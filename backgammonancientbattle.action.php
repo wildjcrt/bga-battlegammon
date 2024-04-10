d0d8f9a636ec836c3aaae1befbcf61eca85e8aa7
wildjcrt <wildjcrt@yahoo.com.tw>
2024-04-10 10:43:26 +0800

    Rename `Battlegammon implementation`

--- backgammonancientbattle.action.php
+++ backgammonancientbattle.action.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
- * backgammonancientbattle implementation : © <Your name here> <Your email address here>
+ * Battlegammon implementation : © <Your name here> <Your email address here>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
